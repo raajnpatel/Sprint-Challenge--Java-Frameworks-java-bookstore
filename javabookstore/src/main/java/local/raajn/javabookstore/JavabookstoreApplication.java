96051eca8eb50fb10f5717bb8b8fbd8870dafbd1
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-18 08:07:59 -0700

    project initiated

---
+++ javabookstore/src/main/java/local/raajn/javabookstore/JavabookstoreApplication.java
@@ -0,0 +1,15 @@
+package local.raajn.javabookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabookstoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavabookstoreApplication.class, args);
+    }
+
+}
